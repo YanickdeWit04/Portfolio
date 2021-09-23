6df66819d786156f8c6a6deedeb8f262c5abdb19
Student Yanick de Wit <ywit0068@student.landstede.nl>
2021-09-23 10:46:54 +0200

    Home pagina layout af, excl foto en tekst

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
     <meta charset="UTF-8">
@@ -8,16 +8,16 @@
     <title>Home</title>
     <link rel="stylesheet" href="/css/style.css">
 </head>
-
+<!---Dit is het hele groene gedeelte van de website, Dus de Navbar, de titel en de dropdown menu.--->
 <body>
-    <div class="test">
+    <div class="top-bar">
         <div class="titel">
             <h1>Yanick de Wit</h1>
             <div class="navbar">
-                <a class="help" href="/">Home</a>
-                <a class="help" href="/overmij">over mij</a>
-                <a class="help" href="/cv">cv</a>
-                <a class="help" href="/contact">contact</a>
+                <a class="margin-nav-bar" href="/">Home</a>
+                <a class="margin-nav-bar" href="/overmij">over mij</a>
+                <a class="margin-nav-bar" href="/cv">cv</a>
+                <a class="margin-nav-bar" href="/contact">contact</a>
                 <div class="dropdown">
                     <button class="dropbtn">Projecten
                         <i class="fa fa-caret-down"></i>
@@ -30,20 +30,16 @@
                 </div>
             </div>
         </div>
-        <div class="line">
-        </div>
     </div>
-    <div class="fototekst1">
-        <p>Hier komt een kort stukje over mijzelf.</p>
+<!---Dit is het eerste block met tekst en een foto. Hier komt een voorbeeld foto en een stukje tekst van een project.--->
+    <div class="voorbeeld1">
+        <p class="voorbeeldtekst1">Carried nothing on am warrant towards. Polite in of in oh needed itself silent course. Assistance travelling so especially do prosperous appearance mr no celebrated. Wanted easily in my called formed suffer. Songs hoped sense as taken ye mirth at. Believe fat how six drawing pursuit minutes far. Same do seen head am part it dear open to. Whatever may scarcely judgment had. Mind what no by kept. Celebrated no he decisively thoroughly. Our asked sex point her she seems. New plenty she horses parish design you. Stuff sight equal of my woody. Him children bringing goodness suitable she entirely put far daughter.</p>
+        <img class="voorbeeldfoto1" src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="Foto Van mijzelf." style="width:700px;Height:150px">
     </div>
-    <div class="foto1">
-        <img src="https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/58374555_876250702706658_6787256042418864128_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lwpW01Y9H-IAX9uJRqh&_nc_ht=scontent-ams4-1.xx&oh=13f0d4fb212c8b6bb31b2bedca557f8a&oe=616861B7" alt="Foto Van mijzelf." style="width:150px;Height:150px">
-    </div>
-    <div class="foto2">
-        <img src="https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/58374555_876250702706658_6787256042418864128_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lwpW01Y9H-IAX9uJRqh&_nc_ht=scontent-ams4-1.xx&oh=13f0d4fb212c8b6bb31b2bedca557f8a&oe=616861B7" alt="foto" style="width:150px;Height:150px">
-    </div>
-    <div class="fototekst2">
-        <p>test</p>
+<!---Dit is het tweede block met een foto en dan een stukje tekst. Hier komt een voorbeeld foto en een korte text van een project.--->
+    <div class="voorbeeld2">
+        <img class="voorbeeldfoto2" src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="foto" style="width:700px;Height:150px">
+        <p class="voorbeeldtekst2">Carried nothing on am warrant towards. Polite in of in oh needed itself silent course. Assistance travelling so especially do prosperous appearance mr no celebrated. Wanted easily in my called formed suffer. Songs hoped sense as taken ye mirth at. Believe fat how six drawing pursuit minutes far. Same do seen head am part it dear open to. Whatever may scarcely judgment had. Mind what no by kept. Celebrated no he decisively thoroughly. Our asked sex point her she seems. New plenty she horses parish design you. Stuff sight equal of my woody. Him children bringing goodness suitable she entirely put far daughter. </p>
     </div>
 
 </body>
