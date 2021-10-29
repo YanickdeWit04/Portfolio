7de5776a9f526e60fe291d33eec1fde47f3f2910
Student Yanick de Wit <ywit0068@student.landstede.nl>
2021-10-29 10:19:48 +0200

    language page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/stylelang.css">
     <title>Document</title>
 </head>
 <body>
     <div>
-        <h1>Chooose your language.</h1>
-            <div>
-                <a href="/home/nl=nl">Dutch</a>
-                <a href="/home/en=en">English</a>
-            <div>
+        <h1 class="test1">Choose your language.</h1>
+    </div>
+    </div class="test">
+        <button style="width: 204px" class="btn"><a class="a" href='/home/nl=nl' alt='Broken Link'>Dutch</button>
+        <button class="btn"><a class="a" href='/home/en=en' alt='Broken Link'>English</a></button>
     </div>
 </body>
 </html>
