5f5606814386e1ca651d2a9f138d06f68157557d
Student Yanick de Wit <ywit0068@student.landstede.nl>
2021-11-03 09:41:21 +0100

    gallery

--- resources/views/nl/grid.blade.php
+++ resources/views/nl/grid.blade.php
@@ -1,4 +1,23 @@
 @extends('parent-blades.inhoudnl')
 @section('content')
-
+<div class="grid">
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <a class="grid-item" href="https://www.w3schools.com"><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
+</div>
 @endsection
