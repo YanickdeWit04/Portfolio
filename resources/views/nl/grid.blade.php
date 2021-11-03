8c0bd52c05584e7a43e84b2982ea00489f95c7bd
Student Yanick de Wit <ywit0068@student.landstede.nl>
2021-11-03 11:15:16 +0100

    project grid echt css responsive

--- resources/views/nl/grid.blade.php
+++ resources/views/nl/grid.blade.php
@@ -1,23 +1,28 @@
 @extends('parent-blades.inhoudnl')
 @section('content')
-<div class="grid">
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <a class="grid-item" href=""><img src="../images/henk.png" alt="W3Schools.com" width="200" height="200"></a>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis unde repellendus. Voluptatibus quisquam similique temporibus quasi odio consequatur ea harum minus velit reprehenderit eligendi asperiores, natus, corrupti mollitia ab!</p>
-</div>
+<div class="grids">
+    <div class="itemgrid">
+        <img src="../images/henk.png" alt="" width="200" height="200">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, necessitatibus quaerat ipsam recusandae unde nesciunt qui dolorum, molestiae excepturi vero, velit molestias quas. Ducimus voluptatem recusandae distinctio consequuntur, cumque nam.</p>
+    </div>
+    <div class="itemgrid">
+        <img src="../images/henk.png" alt="" width="200" height="200">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, necessitatibus quaerat ipsam recusandae unde nesciunt qui dolorum, molestiae excepturi vero, velit molestias quas. Ducimus voluptatem recusandae distinctio consequuntur, cumque nam.</p>
+    </div>
+    <div class="itemgrid">
+        <img src="../images/henk.png" alt="" width="200" height="200">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, necessitatibus quaerat ipsam recusandae unde nesciunt qui dolorum, molestiae excepturi vero, velit molestias quas. Ducimus voluptatem recusandae distinctio consequuntur, cumque nam.</p>
+    </div>
+    <div class="itemgrid">
+        <img src="../images/henk.png" alt="" width="200" height="200">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, necessitatibus quaerat ipsam recusandae unde nesciunt qui dolorum, molestiae excepturi vero, velit molestias quas. Ducimus voluptatem recusandae distinctio consequuntur, cumque nam.</p>
+    </div>
+    <div class="itemgrid">
+        <img src="../images/henk.png" alt="" width="200" height="200">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, necessitatibus quaerat ipsam recusandae unde nesciunt qui dolorum, molestiae excepturi vero, velit molestias quas. Ducimus voluptatem recusandae distinctio consequuntur, cumque nam.</p>
+    </div>
+    <div class="itemgrid">
+        <img src="../images/henk.png" alt="" width="200" height="200">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, necessitatibus quaerat ipsam recusandae unde nesciunt qui dolorum, molestiae excepturi vero, velit molestias quas. Ducimus voluptatem recusandae distinctio consequuntur, cumque nam.</p>
+    </div>
 @endsection
