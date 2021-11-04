b7d4ca2af2abcdb98251dcc4925d158990537711
Student Yanick de Wit <ywit0068@student.landstede.nl>
2021-11-04 09:19:10 +0100

    home en over mij mobile

--- resources/views/nl/overmij.blade.php
+++ resources/views/nl/overmij.blade.php
@@ -1,9 +1,13 @@
 @extends('parent-blades.inhoudnl')
 @section('content')
 <!---Tekst over mij en de foto.--->
-<div class="voorbeeld1">
+    <div class="voorbeeld1">
+        <img class="img" src="../images/henk.png" alt="Foto Van mijzelf.">
         <p class="voorbeeldtekst1">Mijn naam is Yanick de Wit. Op 18 Juli 2004 ben ik geboren in Genemuiden. Sinds jonge leeftijd ben ik al erg geïnteresseerd in computers en andere elektronica, vroeger kwam mijn oom vaak de computer maken en ik mocht graag kijken hoe hij dat deed. Nu bouw ik zelf wel eens computers en volg ik de opleiding Software Developer bij Landstede MBO. Ik ben ook een fanatieke gamer die zowel games speelt op pc als op console. Verder mag ik graag werken en woon ik thuis met ouders en mijn broertje. </p>
-        <img class="voorbeeldfoto1" src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="Foto Van mijzelf." style="width:800px;Height:450px">
+    </div>
+    <div class="desktop1">
+        <p>Carried nothing on am warrant towards. Polite in of in oh needed itself silent course. Assistance travelling so especially do prosperous appearance mr no celebrated. Wanted easily in my called formed suffer. Songs hoped sense as taken ye mirth at. Believe fat how six drawing pursuit minutes far. Same do seen head am part it dear open to. Whatever may scarcely judgment had. Mind what no by kept. Celebrated no he decisively thoroughly. Our asked sex point her she seems. New plenty she horses parish design you. Stuff sight equal of my woody. Him children bringing goodness suitable she entirely put far daughter.</p>
+        <img class="img" src="../images/henk.png" alt="Foto Van mijzelf.">
     </div>
 </body>
 </html>
